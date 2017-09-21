ed9dab735bf10b7211c67284aab8823b35746e51
D7703_30 <d7703_30@D7703-30ui-iMac.local>
2017-09-21 10:15:35 +0900

    plist 사용 핀 3개 찍기

--- 201312356/ViewController.swift
+++ 201312356/ViewController.swift
@@ -7,19 +7,62 @@
 //
 
 import UIKit
+import MapKit
+class ViewController: UIViewController,MKMapViewDelegate{
 
-class ViewController: UIViewController {
-
+    @IBOutlet weak var myMapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locateToCenter()
+        // plist data 가져오기
+        let path = Bundle.main.path(forResource: "ViewPoint", ofType: "plist")
+        print("path=\(String(describing: path))")
+        
+        let contents = NSArray(contentsOfFile: path!)
+        print("contents=\(String(describing: contents))")
+        // pin point를 저장하기 위한 배열 선언
+        var annotations = [MKPointAnnotation]()
+        // content(딕셔너리의 배열)에서 데이터 뽑아오기
+        if let myItems = contents
+        {
+            for item in myItems
+            {
+            let lat = (item as AnyObject).value(forKey: "lat")
+                let long = (item as AnyObject).value(forKey: "long")
+                let title = (item as AnyObject).value(forKey: "title")
+                let subTitle = (item as AnyObject).value(forKey: "subTitle")
+            
+            print("lat = \(String(describing: lat))")
+                
+            let annotation = MKPointAnnotation()
+            
+            let myLat = (lat as! NSString).doubleValue
+            let myLong = (long as! NSString).doubleValue
+            let myTitle = title as? String
+            let mySubTitle = subTitle as? String
+                annotation.coordinate.latitude = myLat
+                annotation.coordinate.longitude = myLong
+                annotation.title = myTitle
+                annotation.subtitle = mySubTitle
+                annotations.append(annotation)
+                
+                myMapView.delegate = self
+            }
+        }else{print("aaa")}
+        myMapView.showAnnotations(annotations, animated: true)
+        myMapView.addAnnotations(annotations)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func locateToCenter(){
+    let center = CLLocationCoordinate2DMake(35.166197, 129.072594)
+    let span = MKCoordinateSpanMake(0.05, 0.05)
+    let region = MKCoordinateRegionMake(center, span)
+        myMapView.setRegion(region, animated: true)
+    }
 
 }
-
